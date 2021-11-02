51d4bf5eff6e13b36965250a4181781ec624097f
Sebastien David <sebastien.david08@gmail.com>
2021-11-02 17:55:55 +0100

    Ajoutez des fichiers projet.

---
+++ MCVArticles/Data/McvArticleContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MCVArticles.Models;
+
+namespace MCVArticles.Data
+{
+    public class McvArticleContext : DbContext
+    {
+        public McvArticleContext(DbContextOptions<McvArticleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Article> Article { get; set; }
+    }
+}
